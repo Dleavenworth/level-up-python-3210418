2dcd25890716bb7436b2ef9da43c971a8b7f62b7
David Leavenworth <dleavenworth3@gmail.com>
2022-11-29 05:56:08 +0000

    sudoku works now

--- src/13 Solve a Sudoku/my_sudoku.py
+++ src/13 Solve a Sudoku/my_sudoku.py
@@ -3,15 +3,23 @@ import math
 def sudoku_solver(full_grid):
     cell = get_empty_cell(full_grid)
 
+    #print(cell)
+
     if not cell:
         return True
    
-    placement = get_valid_placement(cell, full_grid)
+    for i in range(1, 10):
+        placement = get_valid_placement(cell, full_grid, i)
 
-    if not placement:
-        return False
+        if placement:
 
-    return sudoku_solver(full_grid)
+            full_grid[cell[0]][cell[1]] = i
+
+            if sudoku_solver(full_grid):
+                return True
+    
+            full_grid[cell[0]][cell[1]] = 0
+    return False
    
 
 def get_empty_cell(full_grid):
@@ -21,35 +29,30 @@ def get_empty_cell(full_grid):
                 return (i, j)
     return False
 
-def get_valid_placement(cell, full_grid):
-    for iteration in range(1, 10):
-        row_valid = True
-        col_valid = True
-        subgrid_valid = True
-        one_dim_index = cell[0] * 9 + cell[1]
-        x = math.sqrt(one_dim_index)
-        box_index = (one_dim_index % 9) / x + x * (one_dim_index / (9 * x))
-        subgrid_row = box_index % x * x
-        subgrid_col = box_index / x * x
+def get_valid_placement(cell, full_grid, val):
+    one_dim_index = cell[0] * 9 + cell[1]
+    x = math.floor(math.sqrt(one_dim_index))
+    box_index = math.floor(one_dim_index % 9) // x + x * (one_dim_index // (9 * x))
+    #subgrid_row = math.floor(box_index % (x * x))
+    #subgrid_col = box_index // x * x
+    subgrid_row = cell[0] - cell[0] % 3
+    subgrid_col = cell[1] - cell[1] % 3
 
-        for i in range(0, 9):
-            if full_grid[cell[0]][i] == iteration:
-                row_valid = False
-                break
-            if full_grid[i][cell[1]] == iteration:
-                col_valid = False
-                break
+    for i in range(0, 9):
+        if full_grid[cell[0]][i] == val:
+            return False
+        if full_grid[i][cell[1]] == val:
+            return False
 
-        for k in range(subgrid_row, subgrid_row+3):
-            for p in range(subgrid_col, subgrid_col+3):
-                if full_grid[k][p] == iteration:
-                    subgrid_valid = False
-                    break
-
-        if row_valid and col_valid and subgrid_valid:
-            full_grid[cell[0]][cell[1]] = iteration
-            return True
-    return False
+    # for k in range(subgrid_row, subgrid_row+3):
+    #     for p in range(subgrid_col, subgrid_col+3):
+    #         if full_grid[k][p] == val:
+    #             return False
+    for k in range(3):
+        for j in range(3):
+            if full_grid[k+subgrid_row][j+subgrid_col] == val:
+                return False
+    return True
 
 
 grid = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
